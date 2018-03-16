5afe7f5b265bc02b917ef29e99c840bf928ff418
Daniel Con <ldcono05@gmail.com>
2018-03-16 17:33:58 -0600

    Estrucuracion de variable e inicio de header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { ContactmeComponent } from './components/contactme/contactme.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    ProyectComponent,
+    ReferencesComponent,
+    ContactmeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
